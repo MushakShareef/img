1b5f83aacbef6e3042b79ca528180c827560e95e
Mushak Shareef <sivcanopi@gmail.com>
2025-07-17 14:32:06 +0530

    videoFinal1

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+console.log("✅ Vite config loaded..."); // test log
+console.log("✅ vite.config.js is running...");
+
+
 export default defineConfig({
   plugins: [react()],
   server: {
     headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
     },
   },
 });
+
